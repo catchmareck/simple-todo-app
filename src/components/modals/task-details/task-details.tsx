2e4145544bfe2481845354df3ad483e62d0ce35f
Sebastian Kaczmarek <sebos.k10@gmail.com>
2019-11-02 15:15:09 +0100

    Create modals manager and implement it

--- src/components/modals/task-details/task-details.tsx
+++ src/components/modals/task-details/task-details.tsx
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import './task-details.scss';
-import ReactDOM from "react-dom";
-import ModalOverlay from "../modal-overlay";
+import ModalsManager from "../../../services/modals-manager";
 
 class TaskDetailsModal extends Component<any, any> {
 
-    private closing = false;
-    private modal: any = null;
-    private onClose: Function;
+    private modalsManager: ModalsManager;
     private onEditClick: Function;
 
     constructor(props: { show: boolean, onClose?: Function, onEditClick?: Function }) {
@@ -17,45 +14,19 @@ class TaskDetailsModal extends Component<any, any> {
             show: props.show
         };
 
-        this.onClose = props.onClose || (() => {});
+        this.modalsManager = new ModalsManager(this);
         this.onEditClick = props.onEditClick || (() => {});
         this.closeModal = this.closeModal.bind(this);
     }
 
     componentDidUpdate(prevProps: Readonly<any>, prevState: Readonly<any>, snapshot?: any): void {
-
-        if (!this.closing && this.state.show !== this.props.show) {
-            this.setState({ show: this.props.show });
-        }
-
-        if (this.modal === null && this.props.show === true) {
-
-            this.modal = document.createElement('div');
-            this.modal.style.position = 'absolute';
-            this.modal.style.width = '100%';
-            this.modal.style.height = '100%';
-            document.body.prepend(this.modal);
-            ReactDOM.render(<ModalOverlay />, this.modal);
-        }
-
-        if (!this.closing && this.modal && this.props.show === false) {
-            this.closeModal();
-        }
+        
+        this.modalsManager.componentDidUpdate();
     }
 
     closeModal() {
 
-        this.closing = true;
-        this.setState({ show: false }, () => {
-
-            ReactDOM.unmountComponentAtNode(this.modal);
-            document.body.removeChild(this.modal);
-
-            this.modal = null;
-            this.onClose.call(null);
-            this.closing = false
-        });
-
+        this.modalsManager.closeModal();
     }
 
     render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
---
+++ src/services/modals-manager.tsx
@@ -0,0 +1,53 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import ModalOverlay from "../components/modals/modal-overlay";
+
+class ModalsManager {
+    
+    private closing = false;
+    private modal: any = null;
+    private onClose: Function;
+    
+    constructor(private modalComponent: Component<any, any>) {
+
+        this.onClose = modalComponent.props.onClose || (() => {});
+    }
+    
+    componentDidUpdate() {
+        
+        if (!this.closing && this.modalComponent.state.show !== this.modalComponent.props.show) {
+            this.modalComponent.setState({ show: this.modalComponent.props.show });
+        }
+
+        if (this.modal === null && this.modalComponent.props.show === true) {
+
+            this.modal = document.createElement('div');
+            this.modal.style.position = 'absolute';
+            this.modal.style.width = '100%';
+            this.modal.style.height = '100%';
+            document.body.prepend(this.modal);
+            ReactDOM.render(<ModalOverlay />, this.modal);
+        }
+
+        if (!this.closing && this.modal && this.modalComponent.props.show === false) {
+            this.closeModal();
+        }
+    }
+
+    closeModal() {
+
+        this.closing = true;
+        this.modalComponent.setState({ show: false }, () => {
+
+            ReactDOM.unmountComponentAtNode(this.modal);
+            document.body.removeChild(this.modal);
+
+            this.modal = null;
+            this.onClose.call(null);
+            this.closing = false
+        });
+
+    }
+}
+
+export default ModalsManager;
