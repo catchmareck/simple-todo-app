ac2a9d9622f41b0e3b91434bce43e9132f86f969
Sebastian Kaczmarek <sebos.k10@gmail.com>
2019-11-11 17:59:05 +0100

    Add validation to Edit-task form

--- src/components/modals/edit-task/edit-task.tsx
+++ src/components/modals/edit-task/edit-task.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {ChangeEvent, Component, FormEvent} from 'react';
 import './edit-task.scss';
 import ModalsManager from "../../../services/modals-manager";
 
@@ -11,12 +11,109 @@ class EditTaskModal extends Component<any, any> {
         super(props);
 
         this.state = {
-            show: props.show
+            show: props.show,
+            edit: {
+                fields: {
+                    title: '',
+                    description: '',
+                    deadline: '',
+                    assignees: [],
+                    done: false,
+                    list: ''
+                },
+                validation: {
+                    message: '',
+                    titleOk: true,
+                    descriptionOk: true,
+                    deadlineOk: true,
+                    assigneesOk: true,
+                    doneOk: true,
+                    listOk: true
+                }
+            }
         };
 
         this.modalsManager = new ModalsManager(this);
         this.onDeleteClick = props.onDeleteClick || (() => {});
         this.closeModal = this.closeModal.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSelectChange = this.handleSelectChange.bind(this);
+    }
+
+    handleChange(e: ChangeEvent) {
+
+        const target: HTMLInputElement = e.target as HTMLInputElement;
+        const [formType, fieldName] = target.name.split('-');
+
+        const state = this.state[formType];
+        state.fields[fieldName] = target.value;
+        this.setState({ [formType]: state });
+    }
+
+    handleSelectChange(e: ChangeEvent) {
+
+        const target: HTMLSelectElement = e.target as HTMLSelectElement;
+        const [formType, fieldName] = target.name.split('-');
+
+        const state = this.state[formType];
+        state.fields[fieldName] = Array.from(target.selectedOptions,(e) => (e as HTMLOptionElement).value);
+        this.setState({ [formType]: state });
+    }
+
+    handleEdit(e: FormEvent) {
+
+        e.preventDefault();
+
+        const { valid, ...fields } = this.validateEditForm();
+
+        const stateValidation = Object.keys(fields).reduce((res: any, curr: string) => {
+            res[`${curr}Ok`] = (fields as any)[curr];
+            return res;
+        }, {});
+
+        const { edit } = this.state;
+        edit.validation = {
+            message: valid ? '' : 'Please fill all required fields correctly',
+            ...stateValidation
+        };
+        this.setState({ edit });
+
+        if (valid) {
+            this.closeModal();
+        }
+    }
+
+    private validateEditForm(): { valid: boolean, title: boolean, description: boolean, deadline: boolean, assignees: boolean, done: boolean, list: boolean } {
+
+        const { title, description, deadline, list } = this.state.edit.fields;
+
+        const validation = {
+            title: this.requiredFieldValid(title),
+            description: this.requiredFieldValid(description),
+            deadline: this.dateFieldValid(deadline),
+            assignees: true,
+            done: true,
+            list: this.requiredFieldValid(list)
+        };
+
+        return {
+            valid: !Object.values(validation).includes(false),
+            ...validation
+        }
+    }
+
+    private requiredFieldValid(field: string): boolean {
+
+        return Boolean(field) && field.length > 0;
+    }
+
+    private dateFieldValid(field: string): boolean {
+
+        const now = Date.now();
+        const selected = Date.parse(field);
+
+        return Boolean(field) && selected >= now;
     }
 
     componentDidUpdate(prevProps: Readonly<any>, prevState: Readonly<any>, snapshot?: any): void {
@@ -38,27 +135,33 @@ class EditTaskModal extends Component<any, any> {
                     <button className="close delete" onClick={() => this.onDeleteClick()}>Delete</button>
                 </div>
                 <div className="modal-body">
-                    <form className="form">
+                    <form className="form" onSubmit={this.handleEdit}>
+
+                        <p className="text-center invalid-form-message m-0 mt-1">{this.state.edit.validation.message}</p>
 
                         <div className="row">
-                            <input type="text" name="title" placeholder="Task title"/>
-                            <textarea name="description" placeholder="Task description"></textarea>
+                            <input type="text" className={!this.state.edit.validation.titleOk ? "invalid-user-input" : ""} name="edit-title" placeholder="Task title"value={this.state.edit.fields.name} onChange={this.handleChange} />
+                            <textarea className={!this.state.edit.validation.descriptionOk ? "invalid-user-input" : ""} name="edit-description" placeholder="Task description"value={this.state.edit.fields.description} onChange={this.handleChange} ></textarea>
                         </div>
 
                         <div className="row d-flex details-middle-row">
-                            <input type="text" placeholder="Deadline"/>
-                            <input type="checkbox" id="is-done" />
+                            <input type="date" className={!this.state.edit.validation.deadlineOk ? "invalid-user-input" : ""} name="edit-deadline" placeholder="Deadline" value={this.state.edit.fields.deadline} onChange={this.handleChange} />
+                            <input type="checkbox" id="is-done" className={!this.state.edit.validation.doneOk ? "invalid-user-input" : ""} value={this.state.edit.fields.done} onChange={this.handleChange} />
                             <label htmlFor="is-done">Done?</label>
                         </div>
 
+                        <div className="row d-flex">
+                            <p className="text-center invalid-form-message m-0 mt-1 w-100">{!this.state.edit.validation.deadlineOk ? "Deadline should be later than today" : ""}</p>
+                        </div>
+
                         <div className="row">
-                            <select name="assignees" defaultValue={[]} multiple>
+                            <select name="edit-assignees" multiple className={!this.state.edit.validation.assigneesOk ? "invalid-user-input" : ""} value={this.state.edit.fields.assignees} onChange={this.handleSelectChange}>
                                 <option value="" defaultChecked={true} disabled>Assignees</option>
                                 <option value="1">John Doe</option>
                                 <option value="2">Alie Bartner</option>
                                 <option value="3">Evan Hoggs</option>
                             </select>
-                            <select name="list" defaultValue="">
+                            <select name="edit-list" className={!this.state.edit.validation.listOk ? "invalid-user-input" : ""} value={this.state.edit.fields.list} onChange={this.handleChange}>
                                 <option value="" defaultChecked={true} disabled>List</option>
                                 <option value="1">Todo</option>
                                 <option value="2">Bugs</option>
