afe69e65cf19a59d85d98b688c842f815b80b566
Sebastian Kaczmarek <sebos.k10@gmail.com>
2019-11-11 17:34:15 +0100

    Add validation to Create-task form

--- src/components/modals/create-task/create-task.tsx
+++ src/components/modals/create-task/create-task.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {ChangeEvent, Component, FormEvent} from 'react';
 import './create-task.scss';
 import ModalsManager from "../../../services/modals-manager";
 
@@ -10,11 +10,102 @@ class CreateTaskModal extends Component<any, any> {
         super(props);
 
         this.state = {
-            show: props.show
+            show: props.show,
+            create: {
+                fields: {
+                    title: '',
+                    description: '',
+                    deadline: '',
+                    assignees: []
+                },
+                validation: {
+                    message: '',
+                    titleOk: true,
+                    descriptionOk: true,
+                    deadlineOk: true,
+                    assigneesOk: true
+                }
+            }
         };
 
         this.modalsManager = new ModalsManager(this);
         this.closeModal = this.closeModal.bind(this);
+        this.handleCreate = this.handleCreate.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSelectChange = this.handleSelectChange.bind(this);
+    }
+
+    handleChange(e: ChangeEvent) {
+
+        const target: HTMLInputElement = e.target as HTMLInputElement;
+        const [formType, fieldName] = target.name.split('-');
+
+        const state = this.state[formType];
+        state.fields[fieldName] = target.value;
+        this.setState({ [formType]: state });
+    }
+
+    handleSelectChange(e: ChangeEvent) {
+
+        const target: HTMLSelectElement = e.target as HTMLSelectElement;
+        const [formType, fieldName] = target.name.split('-');
+
+        const state = this.state[formType];
+        state.fields[fieldName] = Array.from(target.selectedOptions,(e) => (e as HTMLOptionElement).value);
+        this.setState({ [formType]: state });
+    }
+
+    handleCreate(e: FormEvent) {
+
+        e.preventDefault();
+
+        const { valid, ...fields } = this.validateCreateForm();
+
+        const stateValidation = Object.keys(fields).reduce((res: any, curr: string) => {
+            res[`${curr}Ok`] = (fields as any)[curr];
+            return res;
+        }, {});
+
+        const { create } = this.state;
+        create.validation = {
+            message: valid ? '' : 'Please fill all required fields correctly',
+            ...stateValidation
+        };
+        this.setState({ create });
+
+        if (valid) {
+            this.closeModal();
+        }
+    }
+
+    private validateCreateForm(): { valid: boolean, title: boolean, description: boolean, deadline: boolean, assignees: boolean } {
+
+        const { title, description, deadline } = this.state.create.fields;
+
+        const validation = {
+            title: this.requiredFieldValid(title),
+            description: this.requiredFieldValid(description),
+            deadline: this.dateFieldValid(deadline),
+            assignees: true
+        };
+
+        return {
+            valid: !Object.values(validation).includes(false),
+            ...validation
+        }
+    }
+
+    private requiredFieldValid(field: string): boolean {
+
+        return Boolean(field) && field.length > 0;
+    }
+
+    private dateFieldValid(field: string): boolean {
+
+        const now = Date.now();
+        const selected = Date.parse(field);
+
+        return Boolean(field) && selected >= now;
     }
 
     componentDidUpdate(prevProps: Readonly<any>, prevState: Readonly<any>, snapshot?: any): void {
@@ -36,11 +127,13 @@ class CreateTaskModal extends Component<any, any> {
                     <i className="close" onClick={this.closeModal}>&times;</i>
                 </div>
                 <div className="modal-body">
-                    <form className="form">
-                        <input type="text" name="title" placeholder="Task title"/>
-                        <textarea name="description" placeholder="Task description"></textarea>
-                        <input type="date" name="deadline" placeholder="Deadline"/>
-                        <select name="assignees">
+                    <form className="form" onSubmit={this.handleCreate}>
+                        <p className="text-center invalid-form-message m-0 mt-1">{this.state.create.validation.message}</p>
+                        <input type="text" className={!this.state.create.validation.titleOk ? "invalid-user-input" : ""} name="create-title" placeholder="Task title" value={this.state.create.fields.name} onChange={this.handleChange} />
+                        <textarea className={!this.state.create.validation.descriptionOk ? "invalid-user-input" : ""} name="create-description" placeholder="Task description" value={this.state.create.fields.description} onChange={this.handleChange} ></textarea>
+                        <input type="date" className={!this.state.create.validation.deadlineOk ? "invalid-user-input" : ""} name="create-deadline" placeholder="Deadline" value={this.state.create.fields.deadline} onChange={this.handleChange} />
+                        <p className="text-center invalid-form-message m-0 mt-1">{!this.state.create.validation.deadlineOk ? "Deadline should be later than today" : ""}</p>
+                        <select multiple className={!this.state.create.validation.assigneesOk ? "invalid-user-input" : ""} name="create-assignees" value={this.state.create.fields.assignees} onChange={this.handleSelectChange}>
                             <option value="1">John Doe</option>
                             <option value="2">Alie Bartner</option>
                             <option value="3">Evan Hoggs</option>
