ea3fffca2b1e60104c53b54f2cc56265cd9a5a9a
Sebastian Kaczmarek <sebos.k10@gmail.com>
2019-11-11 18:03:06 +0100

    Add validation to Edit-team settings form

--- src/components/team-settings/team-settings.tsx
+++ src/components/team-settings/team-settings.tsx
@@ -1,7 +1,70 @@
-import React, { Component } from 'react';
+import React, {ChangeEvent, Component, FormEvent} from 'react';
 import './team-settings.scss';
 
 class TeamSettings extends Component<any, any> {
+
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            edit: {
+                fields: {
+                    name: '',
+                    description: ''
+                },
+                validation: {
+                    message: '',
+                    nameOk: true,
+                    descriptionOk: true
+                }
+            }
+        };
+
+        this.handleEdit = this.handleEdit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e: ChangeEvent) {
+
+        const target: HTMLInputElement = e.target as HTMLInputElement;
+        const [formType, fieldName] = target.name.split('-');
+
+        const state = this.state[formType];
+        state.fields[fieldName] = target.value;
+        this.setState({ [formType]: state });
+    }
+
+    handleEdit(e: FormEvent) {
+
+        e.preventDefault();
+
+        const { valid, name } = this.validateCreateForm();
+
+        const { edit } = this.state;
+        edit.validation = {
+            message: valid ? '' : 'Please fill all required fields correctly',
+            nameOk: name,
+            descriptionOk: true
+        };
+        this.setState({ edit });
+    }
+
+    private validateCreateForm(): { valid: boolean, name: boolean } {
+
+        const { name } = this.state.edit.fields;
+
+        const validName = this.requiredFieldValid(name);
+
+        return {
+            valid: validName,
+            name: validName
+        }
+    }
+
+    private requiredFieldValid(field: string): boolean {
+
+        return Boolean(field) && field.length > 0;
+    }
 
     navigateTo(link: string) {
 
@@ -19,8 +82,9 @@ class TeamSettings extends Component<any, any> {
 
                 <div className="row team-settings-body d-flex my-4">
                     <div className="team-details-col">
-                        <form id="team-details-form" className="form pt-0">
-                            <input type="text" name="name" placeholder="Team name" />
+                        <form id="team-details-form" className="form pt-0" onSubmit={this.handleEdit}>
+                            <p className="text-center invalid-form-message m-0 mt-1">{this.state.edit.validation.message}</p>
+                            <input type="text"  className={!this.state.edit.validation.nameOk ? "invalid-user-input" : ""} name="edit-name" placeholder="Team name" value={this.state.edit.fields.name} onChange={this.handleChange} />
                             <textarea name="description" placeholder="Team description"></textarea>
 
                             <button type="submit">Save</button>
